1c0af355b46f4ecb6f521c34d2c58054f7c2cf67
GisatAZK2 <gisatazk2@gmail.com>
2025-07-05 00:02:04 +0700

    Add Config Backend For Frontend

---
+++ server-config.js
@@ -0,0 +1,12 @@
+// server-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDq-9bikCBHU_kaFcT5XLUyVOjGkXowsOI",
+  authDomain: "market-5fdfa.firebaseapp.com",
+  projectId: "market-5fdfa"
+};
+
+const BASE_URL = "https://backendmarket-production.up.railway.app";
+
+const API_KEY = "e16b5b3f9cc54069cabe223571f31cd308e8031225f03d523ddab6ef64f0d4c2";
+
+export { firebaseConfig, BASE_URL, API_KEY };
