12b1c20e5888688ffd5998bc07c9f0f10a307dc7
GisatAZK2 <gisatazk2@gmail.com>
2025-07-16 15:30:47 +0700

    Fix Connect Database Supabase To Railway

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const pool = require('../config/db');
+const pool = require('../config/supabase');
 const { generateOtp, sendPasswordResetEmail } = require('../utils/otp');
 const authMiddleware = require('../middleware/authMiddleware');
 
